15a63cfc2566dfb88fd96480ed380dbff5b791b9
fbuitrago <fabianbuitragov@gmail.com>
2023-02-23 20:56:51 -0500

    add react-router-dom and sidebar, topbar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+
+import Home from "./pages/home/Home";
 import "./App.css";
 
+const AppLayout = () => (
+  <>
+    <Topbar />
+    <div className="container">
+      <Sidebar />
+      <Outlet />
+    </div>
+  </>
+);
+
 function App() {
-  return <h1>Hello World</h1>;
+  const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const [userState, setUserState] = useState([]);
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import "./home.css";
+import { userData } from "../../dummyData";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
