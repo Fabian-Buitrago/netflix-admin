b73d1b5043c9496e9bcaedc0b31e610fa65be164
fbuitrago <fabianbuitragov@gmail.com>
2023-02-24 10:43:13 -0500

    validate access

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
@@ -6,23 +7,31 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import ProductList from "./pages/productList/ProductList";
+import { AuthContext } from "./context/authContext/AuthContext";
 import "./App.css";
 
-const AppLayout = () => (
-  <>
-    <Topbar />
-    <div className="container">
-      <Sidebar />
-      <Outlet />
-    </div>
-  </>
-);
+const AppLayout = ({ admin }) =>
+  admin ? (
+    <>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </>
+  ) : (
+    <Navigate to="/login" replace />
+  );
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route element={<AppLayout />}>
+      <Route
+        path="/login"
+        element={user ? <Navigate to="/" replace /> : <Login />}
+      />
+      <Route element={<AppLayout admin={user} />}>
         <Route index element={<Home />} />
         <Route path="/movies" element={<ProductList />} />
       </Route>
--- src/context/authContext/apiCalls.js
+++ src/context/authContext/apiCalls.js
@@ -4,8 +4,8 @@ import { loginStart, loginFailure, loginSuccess } from "./AuthActions";
 export const login = async (user, dispatch) => {
   dispatch(loginStart);
   try {
-    const res = axios.post("auth/login", user);
-    dispatch(loginSuccess(res.data));
+    const res = await axios.post("auth/login", user);
+    res.data.isAdmin && dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure);
   }
